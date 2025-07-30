1b5581aad001044e204f9224c760e5229eafb937
hyoeun.jeong <jhe0316@naver.com>
2025-07-30 15:16:58 +0900

    회원가입 응답 수정: 프로필 단계 안내 메시지 추가

--- users/views.py
+++ users/views.py
@@ -1,13 +1,20 @@
+# users/views.py
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserSignupSerializer
-
 
 class UserSignupView(APIView):
     def post(self, request):
         serializer = UserSignupSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({"message": "회원가입 성공!"}, status=status.HTTP_201_CREATED)
+            return Response(
+                {
+                    "message": "이메일 등록 완료, 프로필 설정으로 이동하세요.",
+                    "next": "/users/profile/"
+                },
+                status=status.HTTP_201_CREATED
+            )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

5b61e2d0231cea88c768f5bcbbde0d5947b8d4a4
juwan.bae <vitr5316@naver.com>
2025-07-31 14:24:37 +0900

    admin 오류 해결1 모델주석처리2

--- products/admin.py
+++ products/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
 from .models import Product, ProductCategory
 
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'price', 'stock', 'created_at']
-    list_filter = ['created_at']
-    search_fields = ['name', 'seller__username']
-    ordering = ['-created_at']
-
+admin.site.register(Product)
 admin.site.register(ProductCategory)

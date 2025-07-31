8435ee032fb612ec3d6f940ee64fe746d9ae2fa5
juwan.bae <vitr5316@naver.com>
2025-07-31 14:21:58 +0900

    admin 오류 해결1 모델주석처리

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'user', 'created_at']
+    list_filter = ['user', 'created_at']
+    search_fields = ['title', 'content']
+    ordering = ['-created_at']

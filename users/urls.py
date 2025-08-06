73defdbb9e82ed822b73b5e817b6460f0f62ba9b
hyoeun.jeong <jhe0316@naver.com>
2025-08-06 18:34:18 +0900

    myposts view,수정,삭제

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MyPostsView
+from .views import MyPostsView,MyPostDetailView
 
 urlpatterns = [
     path('myposts/', MyPostsView.as_view()), 
+    path('myposts/<int:pk>/', MyPostDetailView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,11 +1,20 @@
-
 from rest_framework import generics, permissions
 from posts.models import Post
 from posts.serializers import PostSerializer
 
+# 1. 내가 쓴 글 리스트 조회
 class MyPostsView(generics.ListAPIView):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
+
     def get_queryset(self):
         return Post.objects.filter(author=self.request.user).order_by('-created_at')
- 
+
+
+# 2. 내가 쓴 글 상세 조회 + 수정 + 삭제
+class MyPostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Post.objects.filter(author=self.request.user)

b461518fe9ffd039228d1b3ac4f0d9e9d6fa92b9
juwan.bae <vitr5316@naver.com>
2025-08-06 15:23:39 +0900

    jwt인증 기능 추가

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,11 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -26,6 +31,9 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('chat/', include('chat.urls')),
     path('categories/', include('categories.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]

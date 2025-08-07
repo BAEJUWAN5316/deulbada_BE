439d2531dc37cb68fe4452b6c88f8aec4912bd54
juwan.bae <vitr5316@naver.com>
2025-08-07 22:39:15 +0900

    urls.py 오류 수정

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path, include
+from django.conf import settings # settings 임포트
+from django.conf.urls.static import static # static 함수 임포트
+
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -10,13 +13,14 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
+# Swagger 설정
 schema_view = get_schema_view(
    openapi.Info(
-      title="Your API",
+      title="들바다 API",
       default_version='v1',
-      description="Test description",
+      description="들바다 프로젝트 API 문서",
       terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@yourproject.local"),
+      contact=openapi.Contact(email="contact@yourdomain.com"),
       license=openapi.License(name="BSD License"),
    ),
    public=True,
@@ -36,39 +40,12 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('chat/', include('chat.urls')),
     path('categories/', include('categories.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+
+    # JWT 인증 관련
+    path('api/token/', TokenObtainPairView.as_as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    from django.urls import path, include
-from django.conf import settings # settings 임포트
-from django.conf.urls.static import static # static 함수 임포트
 
-# drf-yasg (Swagger) 관련 임포트 (필요시 추가)
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-# Swagger 설정
-schema_view = get_schema_view(
-    openapi.Info(
-        title="들바다 API",
-        default_version='v1',
-        description="들바다 프로젝트 API 문서",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@yourdomain.com"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')),
-    path('api/posts/', include('posts.urls')),
-    path('api/products/', include('products.urls')),
-    path('api/chat/', include('chat.urls')),
-    path('api/categories/', include('categories.urls')),
     # drf-yasg (Swagger)
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
@@ -77,4 +54,3 @@ urlpatterns = [
 # 개발 환경에서만 미디어 파일을 서빙하도록 설정
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-]

9adaf95c08773e8fc11f9c49374a59db9db2a085
juwan.bae <vitr5316@naver.com>
2025-08-07 15:59:35 +0900

    products/views.py 오류 수정

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics, permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Product

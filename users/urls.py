00c223c63a36ab9edf28f63ab24f9b8332acd636
hyoeun3n <151709643+hyoeunjeong@users.noreply.github.com>
2025-08-08 16:26:11 +0900

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignupView
+from .views import SignupView, EmailCheckView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
+    path('check-email/', EmailCheckView.as_view(), name='check-email'),  # api 이메일 중복 
 ]

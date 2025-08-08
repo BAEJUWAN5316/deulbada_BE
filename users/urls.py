c031c0193179c6347939fee1815015f0858ffdcd
hyoeun3n <151709643+hyoeunjeong@users.noreply.github.com>
2025-08-08 16:07:50 +0900

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
-from .views import LoginView
+from .views import CustomTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]

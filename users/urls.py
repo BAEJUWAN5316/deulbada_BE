472752e40d6f9d1047ea1d9f1f719bd49809e3e0
hyoeun3n <151709643+hyoeunjeong@users.noreply.github.com>
2025-08-08 10:48:13 +0900

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,17 +1,7 @@
+
 from django.urls import path
-from .views import MyProfileView, FollowToggleView,FollowerListView, FollowingListView
-
-
-
+from .views import LoginView
 
 urlpatterns = [
-    # ✅ 내 프로필 조회/수정
-    path('me/', MyProfileView.as_view(), name='my-profile'),
-
-    # ✅ 팔로우 / 언팔로우 토글 API
-    path('follow/<str:account_id>/', FollowToggleView.as_view(), name='follow-toggle'),
-
-    path('followers/<str:account_id>/', FollowerListView.as_view(), name='follower-list'),
-
-    path('following/<str:account_id>/', FollowingListView.as_view(), name='following-list'),
+    path('login/', LoginView.as_view(), name='login'),
 ]

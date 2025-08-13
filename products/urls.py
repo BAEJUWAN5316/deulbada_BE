be230e2c6c26808f9553954828d90c3ae899cb3a
juwan.bae <vitr5316@naver.com>
2025-08-13 11:56:39 +0900

    product tag기능 별도 추가2

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path('category/<str:category_name>/', ProductListCreateView.as_view(), name='product-list-by-category'),
     path('user/<str:username>/', ProductListCreateView.as_view(), name='product-list-by-user'),
     path('<int:pk>/', ProductRetrieveUpdateDestroyView.as_view(), name='product-retrieve-update-destroy'),
-]
+] 
